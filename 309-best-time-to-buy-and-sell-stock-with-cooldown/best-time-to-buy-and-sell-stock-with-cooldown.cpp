1d3bf4aa11ae63a16a84fb17c3d30ceb03de7d79
Radheshyam Ghanta <146259883+radheshyamghanta2006@users.noreply.github.com>
2025-11-10 23:53:25 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (59.81%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        //Code with Radheshyam (.^.)
+        int n = prices.size();
+        //this->arr = arr;
+        vector<vector<int>> dp(n + 2, vector<int>(2));
+
+        for(int i=n-1;i>=0;i--){
+            dp[i][1] = max(-prices[i] + dp[i+1][0],dp[i+1][1]);
+            dp[i][0] = max(prices[i] + dp[i+2][1],dp[i+1][0]);
+        }
+        return dp[0][1]; 
+    }
+};
